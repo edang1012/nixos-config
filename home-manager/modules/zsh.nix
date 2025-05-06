eec0c3f50673ae4c0ff3c96cf70ae0608754c9c7
Xenosen <43113480+edang1012@users.noreply.github.com>
2025-05-06 18:11:43 -0400

    Update zsh.nix

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -18,7 +18,7 @@
       expireDuplicatesFirst = true;
     };
 
-    initExtra = ''
+    initConfig = ''
       #if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
       #  exec tmux
       #fi
