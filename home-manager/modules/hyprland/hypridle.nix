ac0bd14ff98c7d0ca89301793160a85e75aa6cf5
Xenosen <43113480+edang1012@users.noreply.github.com>
2025-05-06 18:32:10 -0400

    Update hypridle.nix

--- home-manager/modules/hyprland/hypridle.nix
+++ home-manager/modules/hyprland/hypridle.nix
@@ -3,6 +3,7 @@
     enable = true;
     settings = {
       general = {
+        ignore_dbus_inhibit = false;
         before_sleep_cmd = "loginctl lock-session"; 
         after_sleep_cmd = "hyprctl dispatch dpms on";
         lock_cmd = "pidof hyprlock || hyprlock";
