5ee2fc63b88bf355b08630a50b889bd5a704aef4
Xenosen <43113480+edang1012@users.noreply.github.com>
2025-05-06 18:42:25 -0400

    Update home-packages.nix

--- home-manager/home-packages.nix
+++ home-manager/home-packages.nix
@@ -1,9 +1,15 @@
 { pkgs, ... }: {
 
   home.packages = with pkgs; [
+    # fonts
+    noto-fonts-cjk-sans
+
+    # cli
     hello
     btop
     eza
     zoxide
   ];
+
+  fonts.fontconfig.enable = true;
 }
