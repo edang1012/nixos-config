22c07fa65e9ccc2febf0985c992c971a41b4b506
Xenosen <43113480+edang1012@users.noreply.github.com>
2025-05-08 20:08:06 -0400

    Test branch (#6)
    * Create fastfetch.nix
    
    * Update fastfetch.nix
    
    * Update fastfetch.nix
    
    * Create default.nix
    
    * Update home.nix
    
    * Update home.nix
    
    * Update configuration.nix
    
    * Update configuration.nix
    
    * Update configuration.nix
    
    * Update configuration.nix
    
    * Rename defaults.nix to default.nix
    
    * Update flake.nix
    
    * Update flake.nix
    
    * Update flake.nix
    
    * Update home.nix
    
    * Update flake.nix
    
    * Update flake.nix
    
    * Update home.nix
    
    * Update default.nix
    
    * Update home.nix
    
    * Create home-packages.nix
    
    * Update configuration.nix
    
    * Update home-packages.nix
    
    * Update configuration.nix
    
    * Create zsh.nix
    
    * Update default.nix
    
    * Update home-packages.nix
    
    * Update zsh.nix
    
    * Update home-packages.nix
    
    * Update configuration.nix
    
    * Update user.nix
    
    * Update zsh.nix
    
    * Update user.nix
    
    * Update zsh.nix
    
    * Create tmux.nix
    
    * Update tmux.nix
    
    * Update default.nix
    
    * Update tmux.nix
    
    * Update zsh.nix
    
    * Update tmux.nix
    
    * Update configuration.nix
    
    * Create wezterm.nix
    
    * Update wezterm.nix
    
    * Update default.nix
    
    * Update wezterm.nix
    
    * Update default.nix
    
    * Update zsh.nix
    
    * Update zsh.nix
    
    * Update zsh.nix
    
    * Update zsh.nix
    
    * Create default.nix
    
    * Create hypridle.nix
    
    * Update hypridle.nix
    
    * Update default.nix
    
    * Update home-packages.nix
    
    * Create hyprlock.nix
    
    * Create hyprpaper.nix
    
    * Update default.nix
    
    * Update flake.nix
    
    * Update home-packages.nix
    
    * Update zsh.nix
    
    * Update default.nix
    
    * Create hyprland.nix
    
    * Update default.nix
    
    * Update default.nix
    
    * Update default.nix
    
    * Update hyprlock.nix
    
    * Update home-packages.nix
    
    * Update hyprland.nix
    
    * Update default.nix
    
    * Update hyprland.nix
    
    * Update hyprland.nix
    
    * Update hyprland.nix
    
    * Update hyprland.nix
    
    * Update home-packages.nix
    
    * Update hyprland.nix
    
    * Update default.nix
    
    * Update hyprpaper.nix
    
    * Update default.nix
    
    * Update default.nix
    
    * Update hyprland.nix
    
    * Update hyprland.nix
    
    * Update hyprland.nix
    
    * Update home-packages.nix
    
    * Update home-packages.nix
    
    * Update hyprland.nix
    
    * Update hyprland.nix
    
    * Update hyprland.nix
    
    * Update configuration.nix
    
    * Update tmux.nix
    
    * Update tmux.nix
    
    * Update tmux.nix
    
    * Add initial lock file
    
    * Update flake.lock
    
    * Create git.nix
    
    * Update default.nix

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./git.nix
     ./fastfetch.nix
     ./tmux.nix
     ./wezterm.nix
---
+++ home-manager/modules/git.nix
@@ -0,0 +1,6 @@
+{
+  programs.git = {
+    userName = "edang1012";
+    userEmail = "erickdang1012@gmail.com";
+  };
+}
