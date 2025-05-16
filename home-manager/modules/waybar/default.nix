6adb96d882ac3735c9abf866be870f61dcec0f05
Erick Dang <erickdang1012@gmail.com>
2025-05-15 20:11:24 -0400

    Ricing checkpoint

--- home-manager/modules/waybar/default.nix
+++ home-manager/modules/waybar/default.nix
@@ -38,16 +38,16 @@
 	  "format" = "󰃰   {:%a, %b. %d - %I:%M %p}";
           "tooltip-format" = "<tt><small>{calendar}</small></tt>";
           "calendar" = {
-	    "mode" = "month";
+	    "mode" = "year";
 	    "mode-mon-col" = 3;
 	    "weeks-pos" = "right";
 	    "on-scroll" = 1;
 	    "format" = {
-	      "months" = "<span color='#ffead3'><b>{}</b></span>";
-	      "days" = "<span color='#ecc6d9'><b>{}</b></span>";
-	      "weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
-	      "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
-	      "today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
+	      "months" = "<span color='#88c0d0'><b>{}</b></span>";
+	      "days" = "<span color='#d8dee9'><b>{}</b></span>";
+	      "weeks" = "<span color='#a3be8c'><b>W{}</b></span>";
+	      "weekdays" = "<span color='#ebcb8b'><b>{}</b></span>";
+	      "today" = "<span color='#bf616a'><b><u>{}</u></b></span>";
             };
           };
 	  "actions" = {
